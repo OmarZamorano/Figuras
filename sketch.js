99cc5a01320823dd485f82a88cb47905b5603e85
OmarZamorano <126824615+OmarZamorano@users.noreply.github.com>
2023-04-19 12:39:10 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,102 +1,122 @@
-class Figura {
-   constructor(x, y, alto, ancho, vx, vy) {
-    this.posicion = createVector(x,y);
+class Figura 
+{
+  constructor(x,y, alto, ancho, vx, vy) 
+  {
+    this.posicion=createVector(x,y);
     this.alto = alto;
     this.ancho = ancho;
-    this.fillred = 255;
-    this.fillgreen = 87;
-    this.fillblue = 57;
-    this.velocidad = createVector(vx,vy);
+    this.fillred=0;
+    this.fillgreen=255;
+    this.fillblue=154;
+    this.velocidad= createVector(vx,vy);
   }
   update()
   {
-      if (this.posicion.x + this.ancho >= 400)
-        {  
-          let valor = random(3); 
-          this.velocidad.x = this.velocidad.x * -valor;
-         this.velocidad.y = this.velocidad.y * -valor;
-        }
-      this.posicion.add(this.velocidad);
+    //derecha
+    if(this.posicion.x + this.ancho >= 450)
+       {
+         this.velocidad.x=this.velocidad.x * -1
+         this.velocidad.y=this.velocidad.y * 1;
+       }
+    //izquierda
+    if(this.posicion.x + this.ancho <= this.ancho)
+       {
+         this.velocidad.x=this.velocidad.x * -1
+         this.velocidad.y=this.velocidad.y * 1;
+       }
+    //arriba
+    if(this.posicion.y + this.alto <= this.alto)
+       {
+         this.velocidad.x=this.velocidad.x * 1
+         this.velocidad.y=this.velocidad.y * -1;
+       }
+    //abajo
+    if(this.posicion.y + this.alto >= 550)
+       {
+         this.velocidad.x=this.velocidad.x * 1
+         this.velocidad.y=this.velocidad.y * -1;
+       }
+    this.posicion.add(this.velocidad);
   }
-  
 }
 
-class Rectangulo extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
+class Rectangulo extends Figura
+{
+  constructor(x,y, alto, ancho, vx, vy) 
   {
-fill(this.fillred,this.fillgreen,this.fillblue);
-rect(this.posicion.x,this.posicion.y,this.alto,this.ancho);
+    super (x,y, alto, ancho, vx, vy);
   }
-}
-
-class Elipse extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
+  draw()
   {
-fill(this.fillred,this.fillgreen,this.fillblue);
-ellipse(this.posicion.x,this.posicion.y,this.alto,this.ancho);
+    fill(255,100,122);
+    rect(this.posicion.x,this.posicion.y,this.alto,this.ancho);
+    stroke("Black");
+    strokeWeight(3);
   }
 }
 
-var figuras = [];
-var dibujando = 'circulo';
-var btnCirculo = null;
-var btnRectangulo = null;
-
-
-function mouseClicked() {
-  // Se crea un objeto según la opción actual
-if (mouseY > 25)
+class Elipse extends Figura
+{
+  constructor(x,y, alto, ancho, vx, vy) 
   {
-  if (dibujando == 'circulo')
-    figuras.push(new Elipse(mouseX,mouseY,20,20,3,1));
-  else if (dibujando == 'rectangulo')
-    figuras.push(new Rectangulo(mouseX,mouseY,20,20,2,1));
+    super (x,y, alto, ancho, vx, vy);
   }
-
-  return false;
+  draw()
+  {
+    fill(0,255,154);
+    ellipse(this.posicion.x,this.posicion.y,this.alto,this.ancho);
+    stroke("Black");
+    strokeWeight(3);
+  }
 }
 
-function setup() {
-  createCanvas(400, 400);
+var figuras=[];
+var dibujando;
+var btnCirculo=null;
+var btnRectangulo=null;
+function setup() 
+{
+  createCanvas(500, 500);
   
-  btnCirculo = createButton('Circulo');
-  btnCirculo.position(0, 0);
+  btnCirculo= createButton('Circulo')
+  btnCirculo.position(10,0);
   btnCirculo.mousePressed(changeCirculo);
-  btnCirculo.style( 'background-color','#cccccc');
   
-  btnRectangulo = createButton('Rectangulo');
-  btnRectangulo.position(75, 0);
+  btnRectangulo = createButton('Rectangulo')
+  btnRectangulo.position(80,0);
   btnRectangulo.mousePressed(changeRectangulo);
 }
 
 function changeCirculo()
-   {
-     btnCirculo.style( 'background-color','#cccccc');
-     btnRectangulo.style( 'background-color','#f0f0f0');
-     dibujando = 'circulo';
-   }
+{
+  btnCirculo.style('background-color','#cccccc');
+  btnRectangulo.style('background-color','#FFFFFF');
+  dibujando='circulo'
+}
+
 function changeRectangulo()
-   {
-     btnRectangulo.style( 'background-color','#cccccc');
-     btnCirculo.style( 'background-color','#f0f0f0');
-     dibujando = 'rectangulo';
-   }
+{
+  btnRectangulo.style('background-color','#cccccc');
+  btnCirculo.style('background-color','#FFFFFF');
+  dibujando='rectangulo'
+}
 
- 
+function draw() 
+{
+  background(1000);
+  figuras.forEach((fig) =>{fig.draw();fig.update();});
+}
 
-function draw() {
-  background(220);
-  figuras.forEach((fig) => 
-   {
-    fig.draw();
-    fig.update();
-   });
+function mouseClicked() 
+{
+  //se crea un objeto segun la opcion actual
+  if (mouseY>20)
+    {
+     if (dibujando=='circulo')
+  figuras.push(new Elipse(mouseX, mouseY,100,50,2,2));
+  else if (dibujando=='rectangulo')
+  figuras.push(new Rectangulo(mouseX, mouseY,100,50,2,2)); 
+    }
+  // prevent default
+  return false;
 }
